4557d1951bcdc7e64b9a53cf12b4ea3e3594377c
Marian Tohaneanu <tohaneanu.marian@yahoo.com>
2023-10-02 00:12:00 +0200

    update

---
+++ python-scrapy/chocolatescraper/chocolatescraper/pipelines.py
@@ -0,0 +1,53 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
+
+
+class ChocolatescraperPipeline:
+    def process_item(self, item, spider):
+        return item
+
+class PriceToUSDPipeline:
+    gbpToUsdRate = 1.3
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        if adapter.get('price'):
+            floatPrice = float(adapter['price'])
+            adapter['price'] = floatPrice * self.gbpToUsdRate
+            return item
+        else:
+            raise DropItem(f'Missing price in this {item}.')
+        
+class DuplicatesPipeline:
+
+    def __init__(self):
+        self.names_seen = set()
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        if adapter['name'] in self.names_seen:
+            raise DropItem(f'Duplicate item found: {item!r}')
+        else:
+            self.names_seen.add(adapter['name'])
+            return item
+
+# class SavingToMysqlPipeline(object):
+#     def __init__(self):
+#         self.create_connection()
+
+#     def create_connection(self):
+#         self.connection = mysql.connector.connect(
+#             host = 'localhost',
+#             user = 'root',
+#             password = '12345678',
+#             database = 'chocolate_scraping',
+#             port = '3306'
+#         )
+#         self.curr = self.connection.cursor()
